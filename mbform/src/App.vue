762d957ba4fa0ccb655cd8d37b2ca056aac79f12
Fabio Garcia <fabiorgarcia@gmail.com>
2025-03-08 15:08:52 -0300

    Home

--- mbform/src/App.vue
+++ mbform/src/App.vue
@@ -4,8 +4,8 @@ import Home from './components/Home.vue'
 
 <template>
   <div>
-
+    <img src="https://static.mercadobitcoin.com.br/web/img/logos/mb/logo-icon-color.svg" width="80" height="80" alt="Logotipo MB">
   </div>
-  <Home msg="Mercado Bitcoin" />
+  <Home msg="Sua plataforma de investimentos digitais." />
 </template>
 
--- mbform/src/components/Home.vue
+++ mbform/src/components/Home.vue
@@ -11,5 +11,15 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
+  <div class="main-content">
+    <form class="mbc-form">
+
+      <div class="steps">
+        <p>Etapa <span class=""><strong>01</strong> de 05</span></p>
+      </div>
+      <h2>Seja Bem-vindo(a)</h2>
+
+    </form>
+  </div>
 </template>
 
