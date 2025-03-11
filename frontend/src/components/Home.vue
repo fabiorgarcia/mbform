9602d7d98fc815d041ff41fedf436632eb6d2c99
Fabio Garcia <fabiorgarcia@gmail.com>
2025-03-11 06:56:30 -0300

    Frontend e Backend

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -37,13 +37,16 @@
 
 
   onMounted (async ()=>{
-    setTimeout(closeLoading, 1500)
     await fetch("http://localhost:3000/api/services")
     .then(res => res.json())
     .then(res => {
       resdata =  res
       console.log(resdata)
+      setTimeout(closeLoading, 1500)
     })
+    .catch(error => {
+      console.log(error)
+      }) 
   })
 
 
@@ -182,7 +185,6 @@
 
     if (!emailError.value && !naturallegalPersonError.value && !nameError.value && !cpfError.value && !birthError.value && !telephoneError.value && !corporateError.value && !cnpjError.value && !openingError.value && !telephoneCorporateError.value && !passwordError.value ) {
       loadingData.value = true;
-      setTimeout(loadingSuces, 4000)
 
       fetch('http://localhost:3000/api/services', {
         method: 'POST',
@@ -206,6 +208,7 @@
         .then(res => res.json())
         .then(data => {
           console.log(data)
+          setTimeout(loadingSuces, 4000)
         })
         .catch(error => {
         console.log(error)
